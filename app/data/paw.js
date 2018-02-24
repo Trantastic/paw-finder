59fec6f3ff37b32c5ab4622a2683dd6f09771f24
Yvette <Yvette@macs-MacBook-Air.local>
2018-02-24 12:11:05 -0800

    Added dummy data

---
+++ app/data/paw.js
@@ -0,0 +1,14 @@
+var dogPals = [
+	{
+		name: "Chase"
+		photo: "../images/chase.jpeg"
+		score:
+	},
+	{
+		name: "Shadow"
+		photo: "../images/shadow.jpg"
+		score:
+	}
+];
+
+module.exports = dogPals;
