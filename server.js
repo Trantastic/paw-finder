fc54e68113064765e1aa6a3da5697fa2d8efac85
Yvette <Yvette@macs-MacBook-Air.local>
2018-02-25 19:51:46 -0800

    added code to connect to routing files

--- server.js
+++ server.js
@@ -10,3 +10,10 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Points at our route js files
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT: " + PORT);
+});
