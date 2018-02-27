a9f7a59b3cd910fb282d3c96234dfc1229f1035a
Yvette <Yvette@macs-MacBook-Air.local>
2018-02-26 22:24:07 -0800

    Coded logic for dog match

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,32 @@
+var dogPals = require("../data/paw.js");
+
+module.exports = function(app){
+	app.get("/api/friends", function(req, res){
+		res.json(dogPals);
+	});
+
+	app.post("/api/friends", function(req, res){
+		var indexNum = 0;
+		var totalDiffArr = [];
+
+		while(indexNum < dogPals.length){
+			var dogs = dogPals[indexNum].score;
+			var user = req.body.score;
+			var diffArr = [];
+			var totalDiff = 0;
+
+			for(var x = 0; x < dogs.length; x++){
+				var diff = Math.abs(dogs[x] - user[x]);
+				totalDiff += diff;
+			}
+			totalDiffArr.push(totalDiff);
+			indexNum++;
+		}
+		var min = Math.min.apply(null, totalDiffArr);
+		var match = totalDiffArr.indexOf(min);
+
+		console.log(dogPals[match]);
+		res.json(dogPals[match]);
+
+	});
+}
