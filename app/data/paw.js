4fa676beb8986039defffb1c0365da11827cf9aa
Yvette <Yvette@airbears2-10-142-43-96.airbears2.1918.berkeley.edu>
2018-02-27 12:57:38 -0800

    Updated photos

--- app/data/paw.js
+++ app/data/paw.js
@@ -1,13 +1,28 @@
 var dogPals = [
 	{
-		name: "Chase"
-		photo: "../images/chase.jpeg"
-		score:
+		"name": "Chase",
+		"photo": "../images/chase.jpeg",
+		"score": [5,3,4,4,5,1,2,3,3,4]
 	},
 	{
-		name: "Shadow"
-		photo: "../images/shadow.jpg"
-		score:
+		"name": "Shadow",
+		"photo": "../images/shadow.jpg",
+		"score": [2,3,3,4,2,1,5,4,2,4]
+	},
+	{
+		"name": "Jake",
+		"photo": "../images/jake.jpg",
+		"score": [4,3,5,3,3,4,5,5,2,4]
+	},
+	{
+		"name": "Lassie",
+		"photo": "../images/lassie.jpg",
+		"score": [2,1,2,3,1,1,3,2,2,3]		
+	},
+	{
+		"name": "Champion",
+		"photo": "../images/champion.jpg",
+		"score": [3,4,5,5,4,4,5,4,3,4]		
 	}
 ];
 
